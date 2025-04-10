a3e4855641e8dcf3a8bc5ff6751ef13409eb4743
Adam Medeneche <adammdn92@gmail.com>
2025-04-10 05:04:44 +0200

    ajout todo.txt

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amedenec <amedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 04:31:00 by amedenec          #+#    #+#             */
-/*   Updated: 2025/04/10 04:18:36 by amedenec         ###   ########.fr       */
+/*   Updated: 2025/04/10 04:44:58 by amedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	parsing(t_data *data, char **argv)
 	if (ft_atoi(argv[2]) <= 0 || is_num(argv[2]))
 		return (write(2, "please enter valids arguments\n", 31));
 	if (ft_atoi(argv[3]) <= 0 || is_num(argv[3]))
-		return (write(2, "please enter valids arguments\n", 31));
+		return (write(2, "please enter valids arguments\n", 31)); // check si il faut que les arguments soit forcement plus grand que 0 ou pas. exemple 3 50 50 0 est ok ? (si on change rien c'est pas ok)
 	if (ft_atoi(argv[4]) <= 0 || is_num(argv[4]))
 		return (write(2, "please enter valids arguments\n", 31));
 	if (argv[5] && (ft_atoi(argv[5]) < 0 || is_num(argv[5])))
